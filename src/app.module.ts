e15d0790b56ad663ed1dbec880bdef79aa780106
Henrique <ishenriquealbert@gmail.com>
2021-08-04 07:14:12 -0300

    :tada: Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { join } from 'path';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
