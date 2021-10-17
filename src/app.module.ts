9c59798126d22b6c8d24283072be69ab59110cb2
Henrique <ishenriquealbert@gmail.com>
2021-10-17 11:29:53 -0300

    :fire: Remove uploads

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,6 @@ import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ReleasesModule } from './releases/releases.module';
-import { FilesModule } from './files/files.module';
 import { TracksModule } from './tracks/tracks.module';
 
 @Module({
@@ -20,7 +19,6 @@ import { TracksModule } from './tracks/tracks.module';
     UsersModule,
     AuthModule,
     ReleasesModule,
-    FilesModule,
     TracksModule
   ],
   controllers: [],
